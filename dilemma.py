83d13f40cd47d23e046b550d12fc31b148d7a0ee
BenderV <mrbenderv@gmail.com>
2014-03-25 21:32:30 +0100

    add readme, data of the iterations, and basic strategy

--- dilemma.py
+++ dilemma.py
@@ -2,54 +2,102 @@ import math
 import random
 
 """
-Two members of a criminal gang are arrested and imprisoned. Each prisoner is in solitary confinement with no means of speaking to or exchanging messages with the other. The police admit they don't have enough evidence to convict the pair on the principal charge. They plan to sentence both to a year in prison on a lesser charge. Simultaneously, the police offer each prisoner a Faustian bargain. Each prisoner is given the opportunity either to betray the other, by testifying that the other committed the crime, or to cooperate with the other by remaining silent. Here's how it goes:
-If A and B both betray the other, each of them serves 2 years in prison
-If A betrays B but B remains silent, A will be set free and B will serve 3 years in prison (and vice versa)
+Two members of a criminal gang are arrested and imprisoned. Each prisoner is in solitary confinement with no means of speaking to or exchanging messages with the other. The police admit they don't have enough evidence to convict the pair on the principal charge. They plan to sentence both to a year in prison on a lesser charge. Simultaneously, the police offer each prisoner a Faustian bargain. Each prisoner is given the opportunity either to betray (defect) the other, by testifying that the other committed the crime, or to cooperate with the other by remaining silent. Here's how it goes:
+If A and B both defect the other, each of them serves 2 years in prison
+If A defects B but B remains silent, A will be set free and B will serve 3 years in prison (and vice versa)
 If A and B both remain silent, both of them will only serve 1 year in prison (on the lesser charge)
 """
 
+Reward = 3
+Sucker = 0
+Temptation = 5
+Penalty = 1
+
+
+
 class Action(object):
     """docstring for Action"""
-    def __init__(self, arg):
-        super(Action, self).__init__()
-        self.arg = arg
+    def __init__(self):
+        #super(Action, self).__init__()
+        pass
     
     @classmethod
-    def beSilent(cls):
-        return "silent"
+    def cooperate(cls):
+        return "cooperate"
     
     @classmethod
-    def betray(cls):
-        return "betray"
+    def defect(cls):
+        return "defect"
     
     @classmethod
-    def beRandom(cls, probabilityOfSilent = 0.5):
-        if(random.uniform(0,1) < probabilityOfSilent):
-            return "silent"
+    def beRandom(cls, probabilityOfcooperate = 0.5):
+        if(random.uniform(0,1) < probabilityOfcooperate):
+            return "cooperate"
         else:
-            return "betray"
+            return "defect"
 
     # to test...
     @classmethod
     def beHuman(cls):
         # We ask for the player input
         choice = None
-        option = ['silent','betray']
+        option = ['cooperate','defect']
         while choice not in option:
-            choice = raw_input("Your choice, 'silent' or 'betray' ? \n")
+            choice = raw_input("Your choice, 'cooperate' or 'defect' ? \n")
             print "you choose :", choice
         return choice
+
+    # TODO implement
+    @classmethod
+    def titfortat(cls, moveId, history):
+        # Titfortat : cooperate on the first move and play the opponents previous move after that
+        if moveId == 1:
+            return "cooperate"
+        else:
+            print "history-1 : ", history[-1]
+            if history[-1] == (0 or 3):
+                return "cooperate"
+            else: # if history[-1] == (1 or 5): # or -1  
+                return "defect"
+
+
+    # TODO change moveId == 1 to history is empty
+    def grim(self, moveId, history):
+        # Grim : cooperate on the first move, and keep cooperating unless the opponent defects, in which case, defect forever
+        if not history: # empty list
+            self.grim = False # initiate
+            return "cooperate"
         
+        elif not self.grim :
+            # cooperate on the first move, and keep cooperating until it get grim
+            if history[-1] == (1 or 5):
+                self.grim = True
+            return "cooperate"
+        else:
+            return "defect"
+
+    def pavlov(self, moveId, history):
+        # Pavlov: cooperate on the first move, and on subsequent moves, switch strategies if you were punished on the previous move
+        if not history: # empty list 
+            self.strategy = "cooperate" # initiate
+            return self.strategy
+            
+        elif history[-1] == (1 or 5):
+            if self.strategy == "cooperate": self.strategy = "defect"
+            else: self.strategy = "cooperate" 
+        return self.strategy
+
 
 class Prisoner(object):
     """docstring for Prisoner"""
     def __init__(self, name):
         self.name = name
-        self.strategy = Action.beSilent()
+        self.action = Action()
+        self.strategy = Action.beRandom() # default strategy
 
-    def decision(self):
+    def decision(self, moveId):
+        # implement gameId, strategy choice and result will follow ?
         return self.strategy
-
 
 
 class Game(object):
@@ -57,50 +105,55 @@ class Game(object):
     def __init__(self, prisonerA, prisonerB):
         self.prisonerA = prisonerA
         self.prisonerB = prisonerB
+        self.moveId = 0
+        self.data = {'id': [], 'A': [], 'B': []}
 
     def play(self):
-        actionOfA = self.prisonerA.decision()
-        actionOfB = self.prisonerB.decision()   
-        if (actionOfA and actionOfB) == "betray":
-            return "A & B get 2 years"
-        if actionOfA == "betray" and actionOfB == "silent":
-            return "A is free and B get 3 years in prisons"
-        if actionOfA == "silent" and actionOfB == "betray":
-            return " B is free and A get 3 years in prisons"
-        if (actionOfA and actionOfB) == "silent":
-            return "A & B get 1 year"
+        self.moveId += 1
+        actionOfA = self.prisonerA.strategy(self.moveId,self.data['B']) # we give the history of the adversary
+        actionOfB = self.prisonerB.strategy(self.moveId,self.data['A']) # same :)
+
+        print actionOfA, actionOfB
+
+        if (actionOfA and actionOfB) == "defect":
+            # self.prisonerA.punish
+            returnA, returnB = Penalty, Penalty
+            print "A & B get 2 years"
+        elif actionOfA == "defect" and actionOfB == "cooperate":
+            returnA = Temptation
+            returnB = Sucker
+            print "A is free and B get 3 years in prisons"
+        elif actionOfA == "cooperate" and actionOfB == "defect":
+            returnA = Sucker
+            returnB = Temptation
+            print "B is free and A get 3 years in prisons"
+        elif (actionOfA and actionOfB) == "cooperate":
+            returnA, returnB =  Reward, Reward
+            print "A & B get 1 year"
+        else  :
+            print "Error, wrong move"
+            returnA, returnB = -1, -1
+
+        self.data['id'].append(self.moveId)
+        self.data['A'].append(returnA)
+        self.data['B'].append(returnB)
+
 
 
 def main():
     prisonerA = Prisoner("A")
     prisonerB = Prisoner("B")
     game = Game(prisonerA, prisonerB)
+    #prisonerA.strategy = Action.titfortat
+    prisonerA.strategy = prisonerA.action.pavlov
+    prisonerB.strategy = prisonerB.action.grim
+    #print prisonerA.strategy(700)
+    #print "Test = ", prisonerA.decision(700)
     print "A = ", prisonerA.strategy
     print "B = ", prisonerB.strategy
-    print game.play()
-
-    prisonerA.strategy = Action.betray()
-    print "A = ", prisonerA.strategy
-    print "B = ", prisonerB.strategy
-    print game.play()
-
-    prisonerA.strategy = Action.beHuman()
-    print game.play()
-
-def test():
-    prisonerA = Prisoner("A")
-    prisonerB = Prisoner("B")
-    game = Game(prisonerA, prisonerB)
-
-    prisonerA.strategy = Action.beSilent()
-    prisonerB.strategy = Action.beSilent()
-    assert(game.play() == "A & B get 1 year")
-    
-    prisonerA.strategy = Action.betray()
-    prisonerB.strategy = Action.betray()
-    assert(game.play() == "A & B get 2 years")
-
+    for move in xrange(1,10):
+        game.play()
+    print game.data
 
 if __name__ == '__main__':
-    #test()
     main()
---
+++ readme.txt
@@ -0,0 +1,42 @@
+other value
+we will choose the commonly used values T = 5, R = 3, P = 1, and S = 0.
+
+(1) Tit-for-tat: cooperate on the ﬁrst move, and play the opponents previous move after that;
+(2) Grim: cooperate on the ﬁrst move, and keep cooperating unless the opponent defects, in which case, defect forever;
+(3) Pavlov: cooperate on the ﬁrst move, and on subsequent moves, switch strategies if you were punished on the previous move
+
+The Nash equilibrium solution for a two player zero-sum problem
+can be easily obtained as a solution to a convex optimization
+problem. We consider a problem of an iterated zero-sum game
+
+
+Defect or Cooperate
+
+
+?
+
+CC - R: Reward 3
+CD - S: Sucker 0
+DC - T: Temptation 5
+DD - P: Penalty 1
+
+T > R > P > S
+(T+S)/2 < R
+
+
+Why dilemna
+
+// we say "move" for iterative game ?
+The number of moves should not be known to the two palers
+The winner is the player with the highest score in the end
+Usually, there are many players :
+	Round Robin Tournament ??
+
+!!
+Non zero Sum Game
+	Both the players can simultaneously win or lose !!
+ 	=> but you can make a version of it
+
+No universal best strategy
+	depends upon the opponents
+
