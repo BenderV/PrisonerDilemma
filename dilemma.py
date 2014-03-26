0ec0156952125b7c99638d92bed976a1f9240532
BenderV <mrbenderv@gmail.com>
2014-03-26 09:45:23 +0100

    fix game play(), integrate iteration, create overtload constructor for game & clean

--- dilemma.py
+++ dilemma.py
@@ -22,23 +22,22 @@ class Action(object):
         pass
     
     @classmethod
-    def cooperate(cls):
+    def cooperate(cls, *args):
         return "cooperate"
     
     @classmethod
-    def defect(cls):
+    def defect(cls, *args):
         return "defect"
     
     @classmethod
-    def beRandom(cls, probabilityOfcooperate = 0.5):
-        if(random.uniform(0,1) < probabilityOfcooperate):
+    def beRandom(cls, *args):
+        if(random.uniform(0,1) < 0.5):
             return "cooperate"
         else:
             return "defect"
 
-    # to test...
     @classmethod
-    def beHuman(cls):
+    def beHuman(cls, moveId, history):
         # We ask for the player input
         choice = None
         option = ['cooperate','defect']
@@ -47,21 +46,18 @@ class Action(object):
             print "you choose :", choice
         return choice
 
-    # TODO implement
+
     @classmethod
     def titfortat(cls, moveId, history):
         # Titfortat : cooperate on the first move and play the opponents previous move after that
         if moveId == 1:
             return "cooperate"
         else:
-            print "history-1 : ", history[-1]
-            if history[-1] == (0 or 3):
+            if history[-1] in (0, 3):
                 return "cooperate"
             else: # if history[-1] == (1 or 5): # or -1  
                 return "defect"
 
-
-    # TODO change moveId == 1 to history is empty
     def grim(self, moveId, history):
         # Grim : cooperate on the first move, and keep cooperating unless the opponent defects, in which case, defect forever
         if not history: # empty list
@@ -70,8 +66,9 @@ class Action(object):
         
         elif not self.grim :
             # cooperate on the first move, and keep cooperating until it get grim
-            if history[-1] == (1 or 5):
+            if history[-1] in (1, 5):
                 self.grim = True
+                return "defect"
             return "cooperate"
         else:
             return "defect"
@@ -90,48 +87,60 @@ class Action(object):
 
 class Prisoner(object):
     """docstring for Prisoner"""
-    def __init__(self, name):
+    def __init__(self, name, strategy=Action.beRandom):
         self.name = name
         self.action = Action()
-        self.strategy = Action.beRandom() # default strategy
+        self.strategy = strategy # default strategy
 
-    def decision(self, moveId):
+    def decision(self, moveId, history):
         # implement gameId, strategy choice and result will follow ?
-        return self.strategy
+        return self.action.strategy
 
 
 class Game(object):
     """docstring for Game"""
-    def __init__(self, prisonerA, prisonerB):
-        self.prisonerA = prisonerA
-        self.prisonerB = prisonerB
+    def __init__(self, A, B):
+        "initialize with the Prisoners"
+        self.prisonerA = A
+        self.prisonerB = B
         self.moveId = 0
         self.data = {'id': [], 'A': [], 'B': []}
 
-    def play(self):
+    @classmethod
+    def fromStrategy(cls, strategyA, strategyB):
+        "initialize game with the strategy names"
+        prisonerA = Prisoner("A", strategyA)
+        prisonerB = Prisoner("B", strategyB)
+        return cls(prisonerA, prisonerB)
+
+    def play(self, moves): # add number of game
+        for x in xrange(0,moves):
+            self.playAMove()
+
+    def playAMove(self):
         self.moveId += 1
         actionOfA = self.prisonerA.strategy(self.moveId,self.data['B']) # we give the history of the adversary
         actionOfB = self.prisonerB.strategy(self.moveId,self.data['A']) # same :)
 
-        print actionOfA, actionOfB
+        # print "A = ",actionOfA, "B = ", actionOfB
 
-        if (actionOfA and actionOfB) == "defect":
+        if actionOfA == actionOfB == "defect":
             # self.prisonerA.punish
             returnA, returnB = Penalty, Penalty
-            print "A & B get 2 years"
+            # print "A & B get 2 years"
         elif actionOfA == "defect" and actionOfB == "cooperate":
             returnA = Temptation
             returnB = Sucker
-            print "A is free and B get 3 years in prisons"
+            # print "A is free and B get 3 years in prisons"
         elif actionOfA == "cooperate" and actionOfB == "defect":
             returnA = Sucker
             returnB = Temptation
-            print "B is free and A get 3 years in prisons"
+            # print "B is free and A get 3 years in prisons"
         elif (actionOfA and actionOfB) == "cooperate":
             returnA, returnB =  Reward, Reward
-            print "A & B get 1 year"
+            # print "A & B get 1 year"
         else  :
-            print "Error, wrong move"
+            # print "Error, wrong move"
             returnA, returnB = -1, -1
 
         self.data['id'].append(self.moveId)
@@ -139,21 +148,30 @@ class Game(object):
         self.data['B'].append(returnB)
 
 
-
-def main():
+def test():
     prisonerA = Prisoner("A")
     prisonerB = Prisoner("B")
-    game = Game(prisonerA, prisonerB)
-    #prisonerA.strategy = Action.titfortat
-    prisonerA.strategy = prisonerA.action.pavlov
-    prisonerB.strategy = prisonerB.action.grim
-    #print prisonerA.strategy(700)
-    #print "Test = ", prisonerA.decision(700)
-    print "A = ", prisonerA.strategy
-    print "B = ", prisonerB.strategy
+
+    game = Game(Action.cooperate, Action().grim)
     for move in xrange(1,10):
         game.play()
     print game.data
+    assert(game.data['A']==[3, 3, 3, 3, 3, 3, 3, 3, 3])
+    assert(game.data['B']==[3, 3, 3, 3, 3, 3, 3, 3, 3])
+
+
+
+def main():
+    game = Game.fromStrategy(Action().titfortat, Action().beRandom)
+    # game = Game(titfortat, random) #=> create a person name titfortat and pass it (avantage : accumulation des scores ?) ou passer juste la technique
+    # game.prisonerA.strategy = game.prisonerB.action.beRandom # or Action.beRandom
+
+    print "A = ", game.prisonerA.strategy
+    print "B = ", game.prisonerB.strategy
+    game.play(1000)
+    print "Result A = ", sum(game.data['A'])
+    print "Result B = ", sum(game.data['B'])
 
 if __name__ == '__main__':
+    #test()
     main()
